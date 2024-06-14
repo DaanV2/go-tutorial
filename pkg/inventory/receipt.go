de8e2b57005753db49bf1a29384898f6af510db9
DaanV2 <daanverstraten@hotmail.com>
2024-06-14 16:51:58 +0200

    Removing code

--- pkg/inventory/receipt.go
+++
@@ -1,9 +0,0 @@
-package inventory
-
-type Receipt struct {
-	Items []Product `json:"items"`
-}
-
-func (r *Receipt) AddProduct(p Product) {
-	r.Items = append(r.Items, p)
-}
--- pkg/rand/bool.go
+++
@@ -1,6 +0,0 @@
-package rand
-
-// RandomBool generates a random boolean value
-func RandomBool() bool {
-	return Float64(1) >= 0.5
-}
